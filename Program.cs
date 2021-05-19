3eb8ec11955a61c01b5e1f7d05f56a70c17d111d
Giga Bokuchava <mister_giga@live.com>
2021-05-19 22:41:05 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fuel_Georgia_Parser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
