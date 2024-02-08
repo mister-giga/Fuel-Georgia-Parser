6cecf85410b15254fd2d6323c64792fefe9382ea
gigab-im <mister_giga@live.com>
2024-02-08 22:41:15 +0400

    Add 5 retry fetch for rompetrol

--- Services/RompetrolDataParser.cs
+++ Services/RompetrolDataParser.cs
@@ -2,6 +2,7 @@
 using HtmlAgilityPack;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
@@ -17,11 +18,39 @@ namespace Fuel_Georgia_Parser.Services
 
         public override string CompanyColor => "#FF9914";
 
+        private static IEnumerable<KeyValuePair<string,string>> GetHeaders()
+        {
+            yield return new("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
+            yield return new("Sec-Fetch-Dest", "document");
+            yield return new("Sec-Fetch-Mode", "navigate");
+            yield return new("Sec-Fetch-Site", "none");
+            yield return new("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.2.1 Safari/605.1.15");
+        }
+
         public override async Task<Fuel[]> GetActiveFuelsAsync()
+        {
+            for (int i = 1; ; i++) 
+            {
+                try
+                {
+                    return await TryGetActiveFuelsAsync();
+                }
+                catch(IOException)
+                {
+                    if (i == 6)
+                        throw;
+
+                    Console.Write($"Failed rompetrol: N{i}");
+                    await Task.Delay(i * 3000);
+                }
+            }
+        }
+        private async Task<Fuel[]> TryGetActiveFuelsAsync()
         {
             var url = "https://www.rompetrol.ge";
             var httpClient = new HttpClient();
-            httpClient.DefaultRequestHeaders.TryAddWithoutValidation("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36");
+            foreach (var header in GetHeaders())
+                httpClient.DefaultRequestHeaders.TryAddWithoutValidation(header.Key, header.Value);
             var html = await httpClient.GetStringAsync(url);
             var doc = new HtmlDocument();
             doc.LoadHtml(html);
@@ -36,13 +65,14 @@ namespace Fuel_Georgia_Parser.Services
             }).ToArray();
 
 
-            return fuels.Select(x => new Fuel 
+            return fuels.Select(x => new Fuel
             {
                 Key = ConvertFuelNameToKey(x.name),
                 Name = x.name,
                 Price = x.price
             }).ToArray();
         }
+
 
         public override async Task<Location[]> GetLocationsAsync()
         {
