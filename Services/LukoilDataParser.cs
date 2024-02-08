376f6735fa2de75594a473924dcb669225c38b14
gigab-im <156656616+gigab-im@users.noreply.github.com>
2024-02-08 04:54:26 +0400

    Update LukoilDataParser.cs

--- Services/LukoilDataParser.cs
+++ Services/LukoilDataParser.cs
@@ -21,26 +21,39 @@ namespace Fuel_Georgia_Parser.Services
 
         public override async Task<Fuel[]> GetActiveFuelsAsync()
         {
-            var url = "http://www.lukoil.ge/?m=328";
+            var url = "http://www.lukoil.ge/prices-history";
             var web = new HtmlWeb();
             var doc = await web.LoadFromWebAsync(url);
 
-            var fuels = doc.DocumentNode.Descendants("table").First(x => x.HasClass("pricetable")).Descendants("tr").Select(x =>
-            {
-                var tds = x.Descendants("td").ToArray();
-                return (
-                    name: tds[0].InnerText.Trim(),
-                    price: decimal.Parse(tds[1].InnerText.Trim())
-                );
-            }).Where(x=>x.price>0).ToArray();
+            var table = doc.DocumentNode.Descendants("table").First();
+            var tbody = table.Descendants("tbody").First();
+            var tr = tbody.Descendants("tr").Last();
+            var tds = tr.Descendants("td").Skip(1).SkipLast(1).ToArray();
+            var prices = tds.Select(x => decimal.Parse(x.InnerText.Trim())).ToArray();
+
+            var thead = table.Descendants("thead").First();
+            var tr_name = thead.Descendants("tr").First();
+            var ths = tr_name.Descendants("th").Skip(1).SkipLast(1).ToArray();
+            var names = ths.Select(x => x.InnerText.Trim()).ToArray();
+
+            var fuels = names.Zip(prices).Select(x => (name: x.First, price: x.Second)).Where(x => x.price > 0).ToArray();
 
 
             return fuels.Select(x => new Fuel
             {
-                Key = ConvertFuelNameToKey(x.name),
-                Name = x.name,
+                Key = ConvertFuelNameToKey(GetFixedName(x.name)),
+                Name = GetFixedName(x.name),
                 Price = x.price
             }).ToArray();
+
+            static string GetFixedName(string name) => name switch
+            {
+                "Super Ecto" => "ევრო სუპერი",
+                "Premium Avangard" => "პრემიუმ ავანგარდი",
+                "Euro Regular" => "ევრო რეგულარი",
+                "Euro Diesel" => "ევრო დიზელი",
+                _ => throw new Exception($"Unknown name: {name}")
+            };
         }
 
         public override async Task<Location[]> GetLocationsAsync()
